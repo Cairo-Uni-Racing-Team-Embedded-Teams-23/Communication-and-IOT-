8d394ccf0eabe42fac5b34d2876ccb2141c0c7a2
h4z3m <h4z3m.private@gmail.com>
2022-10-19 23:53:26 +0200

    Comments and doc

--- BluePill Drivers/CURT_GPIO/CURT_GPIO_headers/GPIO_INTERFACE.h
+++ BluePill Drivers/CURT_GPIO/CURT_GPIO_headers/GPIO_INTERFACE.h
@@ -11,14 +11,14 @@
 #define GPIO_INTERFACE_H_
 
 /******************************************************************************
-*                              Includes                                       *
-*******************************************************************************/
+ *                              Includes                                       *
+ *******************************************************************************/
 
 #include "std_types.h"
 
 /******************************************************************************
-*                              Definitions                                    *
-*******************************************************************************/
+ *                              Definitions                                    *
+ *******************************************************************************/
 
 #define NUM_OF_PINS_PER_PORT  (15U)
 #define NUM_OF_PORTS          (3U)
@@ -42,7 +42,6 @@
 #define PIN13                 (13U)
 #define PIN14                 (14U)
 #define PIN15                 (15U)
-
 
 #define GPIO_HIGH_PORT        (0xFFFFU)
 #define GPIO_LOW_PORT         (0x0000U)
@@ -94,102 +93,169 @@
 #define OUTPUT_SPEED_50MHZ_AFOD (0b1111U)   /*< Output Alternate function Open-drain 50MHz */
 
 /******************************************************************************
-*                              Type definitions                               *
-*******************************************************************************/
+ *                              Type definitions                               *
+ *******************************************************************************/
 
-typedef enum
-{
-    GPIO_PortA = 0,
-    GPIO_PortB = 1,
-    GPIO_PortC = 2
+typedef enum {
+	GPIO_PortA = 0, GPIO_PortB = 1, GPIO_PortC = 2
 } GPIO_PortNameType;
 
-typedef enum
-{
-    GPIO_PIN_RESET,
-    GPIO_PIN_SET
+typedef enum {
+	GPIO_PIN_RESET, GPIO_PIN_SET
 } GPIO_PinStateType;
 
-
-// typedef enum
-// {
-
-//     GPIO_Mode_INPUT_ANLOG = 0b0000,
-//     GPIO_Mode_INPUT_FLOATING = 0b0100,
-//     GPIO_Mode_INPUT_PULLUP_PULLDOWN = 0b100,
-
-//     GPIO_Mode_OUTPUT_SPEED_10MHZ_PP = 0b0001,
-//     GPIO_Mode_OUTPUT_SPEED_10MHZ_OD = 0b0101,
-//     GPIO_Mode_OUTPUT_SPEED_10MHZ_AFPP = 0b1001,
-//     GPIO_Mode_OUTPUT_SPEED_10MHZ_AFOD = 0b110,
-//     GPIO_Mode_OUTPUT_SPEED_2MHZ_PP =  0b0010,
-//     GPIO_Mode_OUTPUT_SPEED_2MHZ_OD = 0b0110,
-//     GPIO_Mode_OUTPUT_SPEED_2MHZ_AFPP = 0b1010,
-//     GPIO_Mode_OUTPUT_SPEED_2MHZ_AFOD = 0b111,
-//     GPIO_Mode_OUTPUT_SPEED_50MHZ_PP = 0b0011,
-//     GPIO_Mode_OUTPUT_SPEED_50MHZ_OD = 0b0111,
-//     GPIO_Mode_OUTPUT_SPEED_50MHZ_AFPP = 0b1011,
-//     GPIO_Mode_OUTPUT_SPEED_50MHZ_AFOD = 0b1111
-// }GPIO_ModeType2;
-
 /******************************************************************************
-*                              Function Prototypes                            *
-*******************************************************************************/
+ *                              Function Prototypes                            *
+ *******************************************************************************/
 
-/*
- * Description :
- * Setup the Mode & Direction of the required pin from 16 Modes.
- * If the input port number or pin number are not correct, The function will not handle the request.
+/**
+ * @fn void GPIO_setupPinMode(uint8, uint8, uint8)
+ * @brief Setup the Mode & Direction of the required pin from 16 Modes.
+ * 		If the input port number or pin number are not correct, The function will not handle the request.
+ *
+ * @pre port_num and pin_num are valid
+ * @post Pin is setup using the mode value selected.
+ * @param port_num Port number
+ * @param pin_num  Pin number
+ * @param mode	   Mode used to set the pin
  */
-
 void GPIO_setupPinMode(uint8 port_num, uint8 pin_num, uint8 mode);
 
-/*
- * Description :
- * Write the value Logic High or Logic Low on the required pin.
+/**
+ * @fn void GPIO_setPinValue(uint8, uint8, uint8)
+ * @brief Write the value Logic High or Logic Low on the required pin.
  * If the input port number or pin number are not correct, The function will not handle the request.
  * If the pin is input, this function will enable/disable the internal pull-up/pull-down resistor.
+ * @pre port_num and pin_num are valid
+ * @post If value is non-zero, then logic high is written to the pin.
+ * @param port_num Port number
+ * @param pin_num  Pin number
+ * @param value	   Value to set the pin state
  */
 void GPIO_setPinValue(uint8 port_num, uint8 pin_num, uint8 value);
 
-/*
- * Description :
- * Read and return the value for the required pin, it should be Logic High or Logic Low.
+/**
+ * @fn uint8 GPIO_getPinValue(uint8, uint8)
+ * @brief  Read and return the value for the required pin, it should be Logic High or Logic Low.
  * If the input port number or pin number are not correct, The function will return Logic Low.
+ * @pre port_num and pin_num are valid
+ * @post Pin value whether it is logic high or low
+ * @param port_num Port number
+ * @param pin_num  Pin number
+ * @return Pin value
  */
 uint8 GPIO_getPinValue(uint8 port_num, uint8 pin_num);
 
-/*
- * Description :
- * Setup the direction of the required port all pins from 16 modes.
+/**
+ * @fn void GPIO_setupPortMode(uint8, uint8)
+ * @brief Setup the direction of the required port all pins from 16 modes.
  * If the direction value is PORT_INPUT all pins in this port should be input pins.
  * If the direction value is PORT_OUTPUT all pins in this port should be output pins.
  * If the input port number is not correct, The function will not handle the request.
+ * @pre port_num is valid
+ * @post All pins on the selected port are setup using the specified mode.
+ * @param port_num Port number
+ * @param mode     Mode used to set all pins on the port
  */
 void GPIO_setupPortMode(uint8 port_num, uint8 mode);
 
-/*
- * Description :
- * Write the value on the required port.
+/**
+ * @fn void GPIO_writePort(uint8, uint16)
+ * @brief  Write the value on the required port.
  * If any pin in the port is output pin the value will be written.
  * If any pin in the port is input pin this will activate/deactivate the internal pull-up resistor.
  * If the input port number is not correct, The function will not handle the request.
+ *
+ * @pre port_num is valid
+ * @post The value on the port becomes the value variable
+ * @param port_num Port number
+ * @param value	   Value used to write to the port
  */
 void GPIO_writePort(uint8 port_num, uint16 value);
 
-/*
- * Description :
- * Read and return the value of the required port.
+/**
+ * @fn uint16 GPIO_getPort(uint8)
+ * @brief  Read and return the value of the required port.
  * If the input port number is not correct, The function will return ZERO value.
+ *
+ * @pre port_num is valid
+ * @post Value on the port
+ * @param port_num Port number
+ * @return Value on the port
  */
 uint16 GPIO_getPort(uint8 port_num);
 
+/**
+ * @fn void GPIO_togglePinValue(uint8, uint8)
+ * @brief Toggles the pin value.
+ *
+ * @pre port_num must be a valid port, and pin_num must be less than or equal to NUM_OF_PINS_PER_PORT
+ * @post Pin value is toggled
+ * @param port_num Port number
+ * @param pin_num  Pin number
+ */
 void GPIO_togglePinValue(uint8 port_num, uint8 pin_num);
 
+/**
+ * @fn void GPIO_enablePortClock(uint8)
+ * @brief Enables the clock for a specified port
+ *
+ * @pre port_num must be a valid port from the available enum
+ * @post Clock for the specified port is started
+ * @param port_num	Port on which to enable the clock
+ */
 void GPIO_enablePortClock(uint8 port_num);
 
+/**
+ * @fn void GPIO_setPortDirection_H_L(uint8, uint8, uint8)
+ * @brief Sets up half of the port with a specified mode. If the position is GPIO_LOW_PORT, then
+ * 		the lower part of the port (pins 0-7) are setup. Otherwise, the higher part of the port (pins 8-15)
+ * 		are setup
+ * 	**Note: The user is responsible for selecting a correct mode and position.
+ * @pre   port_num must be valid
+ * @post  Half of the selected port is setup using the specified mode.
+ * @param port_num Port number
+ * @param position Lower or higher part of the port
+ * @param mode	   Mode used to setup the port half
+ */
 void GPIO_setPortDirection_H_L(uint8 port_num, uint8 position, uint8 mode);
 
+/**
+ * @fn void GPIO_setPortValue_H_L(uint8, uint8, uint16)
+ * @brief Sets up half of the port with the passed value. If the position is GPIO_LOW_PORT, then
+ * 		the lower part of the port (pins 0-7) are set. Otherwise, the higher part of the port (pins 8-15)
+ * 		are set
+ *
+ * @pre   port_num must be valid
+ * @post  Half of the selected port contains the LOWER NIBBLE of value variable.
+ * @param port_num Port number
+ * @param position Lower or higher part of the port
+ * @param value	   Value to set
+ */
 void GPIO_setPortValue_H_L(uint8 port_num, uint8 position, uint16 value);
+
+/**
+ * @fn void GPIO_lockPin(uint8, uint16)
+ * @brief Locks the pin selected. Any modification on the pin mode or value will not take
+ * 		effect until next reset.
+ *
+ * @pre port_num must be valid. Pins is the bit-wise OR'ing of the bits to lock.
+ * 		Ex. : pin 0, pin 4, pin13 -> pins=(1<<0)|(1<<4)|(1<<13);
+ * @post The pins specified are locked till next reset.
+ * @param port_num	Port number
+ * @param pins		Bit-wise OR'ed pins needed to be locked
+ */
+void GPIO_lockPin(uint8 port_num, uint16 pins);
+
+/**
+ * @fn void GPIO_lockPort(uint8)
+ * @brief Locks the port selected. Any modification on the port mode or value will not take
+ * 		effect until next reset.
+ *
+ * @pre port_num must be valid.
+ * @post All of the port pins are locked till next reset.
+ * @param port_num Port number
+ */
+void GPIO_lockPort(uint8 port_num);
 
 #endif
--- BluePill Drivers/CURT_RCC/CURT_RCC_headers/RCC_interface.h
+++ BluePill Drivers/CURT_RCC/CURT_RCC_headers/RCC_interface.h
@@ -25,7 +25,9 @@ typedef enum
 } RCC_Bus;
 
 void RCC_voidInitSysClock(void);
+
 void RCC_voidEnableClock(RCC_Bus Copy_u8BusId, u8 Copy_u8PerId);
+
 void RCC_voidDisableClock(RCC_Bus Copy_u8BusId, u8 Copy_u8PerId);
 
 #endif
