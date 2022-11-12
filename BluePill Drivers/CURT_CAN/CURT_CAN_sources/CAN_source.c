95fdc42224ad3303b6034825c9c0866c2592c09f
AbdelrahmanYousryy <115631933+AbdelrahmanYousryy@users.noreply.github.com>
2022-11-12 03:48:50 +0200

    Update CAN_source.c

--- BluePill Drivers/CURT_CAN/CURT_CAN_sources/CAN_source.c
+++ BluePill Drivers/CURT_CAN/CURT_CAN_sources/CAN_source.c
@@ -1,20 +1,17 @@
-/*******************************************************************************
- *
- * 	Module			: CAN BUS
- *
- * 	File Name		: CAN_source.c
- *
- * 	Author			:
- *
- *	Date 			:
- *
- *	Version			: v1.0
- *
+ /*******************************************************************************
+ *																
+ * 	Module			: CAN BUS									
+ *																
+ * 	File Name		: CAN_source.c										
+ *																	
+ * 	Author			: 						
+ *																
+ *	Date 			: 									
+ *																
+ *	Version			: v1.0										
+ * 																
  *******************************************************************************/
 
-/*******************************************************************************
- *                                Includes                                     *
- *******************************************************************************/
 
 #include "BIT_MATH.h"
 #include "STD_TYPES.h"
@@ -22,109 +19,117 @@
 #include "CAN_config.h"
 #include "CAN_interface.h"
 #include "RCC_interface.h"
-#include "GPIO_INTERFACE.h"
 
-/*******************************************************************************
- *                        Public functions definitions                         *
- *******************************************************************************/
 
-void CAN_init(CAN_TypeDef *CANx, CAN_TypeDef_Config Copy_enuCANConfig)
+void CAN_init(CAN_TypeDef* CANx , CAN_TypeDef_Config Copy_enuCANConfig)
 {
-    /* Instance check */
-    if (!IS_CAN_INSTANCE(CANx))
-        return;
-
-    /* Enable CAN clock */
-    RCC_voidEnableClock(APB1, 25);
-
-    /* Set up GPIO pins as Alternate function mode for CAN pins */
-    GPIO_enablePortClock(GPIOA_ID);
-    GPIO_setupPinMode(GPIOA_ID,PIN11,OUTPUT_SPEED_10MHZ_AFPP);
-    GPIO_setupPinMode(GPIOA_ID,PIN12,OUTPUT_SPEED_10MHZ_AFPP);
-
-    switch (Copy_enuCANConfig)
-    {
-    case CAN_CONFIG_1:
-
-        /* Disable all CAN interrupts */
-        CANx->IER = 0;
-
-        /* Set the bits for settings we need in configuration 1*/
-        CANx->MCR = ((CAN1_FIFO_PRIORITY << MCR_TXFP) |
-                     (CAN1_RECEIVE_FIFO_LOCKED_MODE << MCR_RFLM) |
-                     (CAN1_NO_AUTOMATIC_RETRANSMISSION << MCR_NART) |
-                     (CAN1_AUTOMATIC_WAKE_UP_MODE << MCR_AWUM) |
-                     (CAN1_AUTOMATIC_BUS_OFF << MCR_ABOT) |
-                     (CAN1_TIME_TRIGGERED_COMMUNICATION_MODE << MCR_TTCM));
-
-        /* Exit sleep mode */
-        CLR_BIT(CANx->MCR, MCR_SLEEP);
-        /* Wait until HW is in initialization  mode, so that we can setup timing parameters:
-           INAK = 1
-           SLAK = 0
-        */
-        while (
-            GET_BIT(CANx->MSR, MSR_INAK) &&
-            !GET_BIT(CANx->MSR, MSR_SLAK))
-            ;
-
-        /* Set up timing parameters */
-        CANx->BTR = ((CAN1_MODE << BTR_LBKM) |
-                     (CAN1_RESYNC_JUMP_WIDTH << BTR_SJW_2BITS) |
-                     (CAN1_TIME_SEGMENT_1 << BTR_TS1_4BITS) |
-                     (CAN1_TIME_SEGMENT_2 << BTR_TS2_3BITS) |
-                     (CAN1_BAUD_RATE_PRESCALER << BTR_BRP_10BITS));
-
-        break;
-    case CAN_CONFIG_2:
-
-        /* Disable all CAN interrupts */
-        CANx->IER = 0;
-
-        /* Set the bits for settings we need in configuration 1*/
-        CANx->MCR = ((CAN2_FIFO_PRIORITY << MCR_TXFP) |
-                     (CAN2_RECEIVE_FIFO_LOCKED_MODE << MCR_RFLM) |
-                     (CAN2_NO_AUTOMATIC_RETRANSMISSION << MCR_NART) |
-                     (CAN2_AUTOMATIC_WAKE_UP_MODE << MCR_AWUM) |
-                     (CAN2_AUTOMATIC_BUS_OFF << MCR_ABOT) |
-                     (CAN2_TIME_TRIGGERED_COMMUNICATION_MODE << MCR_TTCM));
-
-        /* Exit sleep mode */
-        CLR_BIT(CANx->MCR, MCR_SLEEP);
-        /* Wait until HW is in initialization  mode, so that we can setup timing parameters:
-           INAK = 1
-           SLAK = 0
-        */
-        while (
-            GET_BIT(CANx->MSR, MSR_INAK) &&
-            !GET_BIT(CANx->MSR, MSR_SLAK))
-            ;
-
-        /* Set up timing parameters */
-        CANx->BTR = ((CAN2_MODE << BTR_LBKM) |
-                     (CAN2_RESYNC_JUMP_WIDTH << BTR_SJW_2BITS) |
-                     (CAN2_TIME_SEGMENT_1 << BTR_TS1_4BITS) |
-                     (CAN2_TIME_SEGMENT_2 << BTR_TS2_3BITS) |
-                     (CAN2_BAUD_RATE_PRESCALER << BTR_BRP_10BITS));
-
-        break;
-    default:
-        break;
-    }
+	
 }
 
 void CAN_initFilter(CAN_FilterInitTypeDef *PTR_sFilterInit)
 {
+		
 }
 
-void CAN_setSlaveStartBank(u8 Copy_u8CANBankNumber)
+void CAN_setSlaveStartBank(u8 Copy_u8CANBankNumber) 
 {
+ 
 }
 
-CAN_Tx_MailBox_TypeDef CAN_transmit(CAN_TypeDef *CANx, CanTxMsg *TxMessage)
+
+CAN_Tx_MailBox_TypeDef CAN_transmit(CAN_TypeDef* CANx, CanTxMsg* TxMessage)
 {
+	
+	/* Transmisson Handling Function */
+
+	/* First Step: Select an Empty Mailbox to Use for Transmission */
+
+	CAN_TxMailBox_TypeDef Local_CAN_TxMailBox_TypeDef_CurrentMailBox;  // variable to save the used MailBox in
+
+	/* Check Which MailBox is Empty to use */
+	if (GET_BIT(CANx->TSR , TSR_TME0))
+	{
+		Local_CAN_TxMailBox_TypeDef_CurrentMailBox = CAN_TX_MAILBOX_1;
+	}
+	else if (GET_BIT(CANx->TSR , TSR_TME1))
+	{
+		Local_CAN_TxMailBox_TypeDef_CurrentMailBox = CAN_TX_MAILBOX_2;
+	}
+	else if (GET_BIT(CANx->TSR , TSR_TME2))
+	{
+		Local_CAN_TxMailBox_TypeDef_CurrentMailBox = CAN_TX_MAILBOX_3;
+	}
+	else
+	{
+		/* All MAILBOXES Are Currently In Use */
+		Local_CAN_TxMailBox_TypeDef_CurrentMailBox = CAN_TX_NO_EMPTY_MAILBOX;
+		return Local_CAN_TxMailBox_TypeDef_CurrentMailBox;
+	}
+
+	/* Second Step: Setup the Identifier
+	 * 				Setup The RTR
+	 * 				Setup the DLC
+	 * 				Put the Data
+	 */
+
+	/* Choose Whether Its Standard or Extended Frame */
+	WRITE_BIT(CANx->sTxMailBox[Local_CAN_TxMailBox_TypeDef_CurrentMailBox].TIR , TIR_IDE , TxMessage->IDE);
+
+	/*Choose Whether Its Remote Frame or No */
+	WRITE_BIT(CANx->sTxMailBox[Local_CAN_TxMailBox_TypeDef_CurrentMailBox].TIR , TIR_RTR , TxMessage->RTR);
+
+	/* Setup The Identifier */
+
+	//Standard Identifier is 11 Bits
+	if(TxMessage->IDE  == CAN_STANDARD_IDENTIFIER)
+	{
+		CANx->sTxMailBox[Local_CAN_TxMailBox_TypeDef_CurrentMailBox].TIR |= ((TxMessage->StdId&0x7FF)<< TIR_STID_11BITS);
+	}
+	//Extended Identifier is 29 Bits
+	else if(TxMessage->IDE  == CAN_EXTENDED_IDENTIFIER)
+	{
+		CANx->sTxMailBox[Local_CAN_TxMailBox_TypeDef_CurrentMailBox].TIR |= ((TxMessage->ExtId&0x1FFFFFFF)<< TIR_EXID_17BITS);
+	}
+
+	/* Setting The DLC*/
+	/* Choose Data length 8 Bytes */
+
+	u8 Local_u8DLC = 0b1111;
+	CANx->sTxMailBox[Local_CAN_TxMailBox_TypeDef_CurrentMailBox].TDTR &= 0xFFFFFFF0;
+	CANx->sTxMailBox[Local_CAN_TxMailBox_TypeDef_CurrentMailBox].TDTR |= TxMessage->DLC;
+
+
+	//Setting the Data in the Message
+
+
+	CANx->sTxMailBox[Local_CAN_TxMailBox_TypeDef_CurrentMailBox].TDHR = (	((u32)TxMessage->Data[7] << 24) |
+																			((u32)TxMessage->Data[6] << 16) |
+																			((u32)TxMessage->Data[5] << 8)  |
+																			((u32)TxMessage->Data[4]));
+
+	CANx->sTxMailBox[Local_CAN_TxMailBox_TypeDef_CurrentMailBox].TDLR = (	((u32)TxMessage->Data[3] << 24) |
+																		((u32)TxMessage->Data[2] << 16) |
+																		((u32)TxMessage->Data[1] << 8)  |
+																		((u32)TxMessage->Data[0]));
+
+
+
+
+
+	/* Final Step: Request The Transmission*/
+	SET_BIT(CANx->sTxMailBox[Local_CAN_TxMailBox_TypeDef_CurrentMailBox].TIR , TIR_TXRQ);
+
+
+	/* Return The used MailBox*/
+	return Local_CAN_TxMailBox_TypeDef_CurrentMailBox;
+
+
+
+
+
 }
 
-void CAN_receive(CAN_TypeDef *CANx, u8 FIFONumber, CanRxMsg *RxMessage)
+void CAN_receive(CAN_TypeDef* CANx, u8 FIFONumber, CanRxMsg* RxMessage)
 {
+	
 }
