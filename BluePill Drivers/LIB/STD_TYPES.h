155c28270faa5fd4129556c3fec08d4ddbd2e74e
h4z3m <h4z3m.private@gmail.com>
2023-08-19 07:50:53 +0300

    ifndef guards to avoid conflicts

--- BluePill Drivers/LIB/STD_TYPES.h
+++ BluePill Drivers/LIB/STD_TYPES.h
@@ -1,52 +1,81 @@
- /*******************************************************************************
- *																				*
- * 	Module			: Standard Types										    *
- *																				*
- * 	File Name		: STD_TYPES.h												*		
- *																				*	
- * 	Author			: AbdElRahman Sabry											*
- *																				*
- *	Date 			: 23/9/2021													*
- *																				*
- *	Version			: v1														*
- * 																				*
+/*******************************************************************************
+ *                                                                                *
+ *     Module          : Standard Types                                            *
+ *                                                                                *
+ *     File Name       : STD_TYPES.h                                               *
+ *                                                                                *
+ *     Author          : AbdElRahman Sabry                                         *
+ *                                                                                *
+ *    Date            : 23/9/2021                                                 *
+ *                                                                                *
+ *    Version         : v1                                                        *
+ *                                                                                 *
  *******************************************************************************/
 
 #ifndef STDTYPES_H_
 #define STDTYPES_H_
 
+#ifdef __cplusplus
+extern "C"
+{
+#endif
 
-//Standard Data Types 
-typedef		unsigned char			u8;
-typedef		char					s8;
-typedef		unsigned short			u16;
-typedef		short					s16;
-typedef		unsigned int			u32;
-typedef		int						s32;
-typedef		unsigned long long		u64;
-typedef		long long				s64;
-typedef		float					f32;
-typedef		double					f64;
+    // Standard Data Types
+    typedef unsigned char u8;
+    typedef signed char s8;
+    typedef unsigned short u16;
+    typedef signed short s16;
+    typedef unsigned int u32;
+    typedef signed int s32;
+    typedef unsigned long long u64;
+    typedef signed long long s64;
+    typedef float f32;
+    typedef double f64;
 
+// Booleans
+#ifndef __cplusplus
+    typedef unsigned char bool;
+#endif
 
+#ifndef TRUE
+#define TRUE (1u)
+#endif
 
-//Booleans 
-typedef		unsigned char			bool;
-#define		TRUE					(1u)
-#define		FALSE					(0u)
-		
-#define		HIGH					(1u)
-#define		LOW						(0u)
+#ifndef FALSE
+#define FALSE (0u)
+#endif
 
-#define		ONE						(1u)
-#define		ZERO					(0u)
+#ifndef HIGH
+#define HIGH (1u)
+#endif
 
+#ifndef LOW
+#define LOW (0u)
+#endif
 
-//Pointers 
-#define		NULLPTR					((void*)0)
+#ifndef ONE
+#define ONE (1u)
+#endif
 
+#ifndef ZERO
+#define ZERO (0u)
+#endif
+
+// Pointers
+#ifndef NULLPTR
+#define NULLPTR ((void *)0)
+#endif
+
+#ifndef DISABLE
 #define DISABLE (0U)
-#define ENABLE (1U)
+#endif
 
+#ifndef ENABLE
+#define ENABLE (1U)
+#endif
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif /* STDTYPES_H_ */
